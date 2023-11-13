1558a7d8d4c6aaa8cc63bf84fb091605547313fc
Aayushchou <aayushchou@gmail.com>
2023-11-13 02:36:44 +0000

    component based refactor

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -1,16 +1,54 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+SliderComponent::SliderComponent(juce::String paramID, juce::String labelText, juce::AudioProcessorValueTreeState &vts)
+{
+    label.setText(labelText, juce::dontSendNotification);
+    slider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    slider.setTextBoxStyle(juce::Slider::NoTextBox, false, 45, 0);
+    slider.setPopupDisplayEnabled(true, true, this);
+    addAndMakeVisible(&label);
+    addAndMakeVisible(&slider);
+    attachment.reset(new juce::AudioProcessorValueTreeState::SliderAttachment(vts, paramID, slider));
+}
+
+SliderComponent::~SliderComponent()
+{
+}
+
+void SliderComponent::paint(juce::Graphics &)
+{
+}
+
+void SliderComponent::resized()
+{
+    auto area = getLocalBounds(); // Get the local bounds of the component
+
+    // Define the height for the label and the margin space for the hover text box
+    auto labelHeight = area.getHeight() / 4; // Allocate a portion of the height to the label
+    auto margin = 10;                        // Space for the hover text box
+
+    // Adjust the area to leave space at the bottom for the hover text box
+    area.reduce(0, margin);
+
+    // Set the label bounds to the top portion and center it horizontally
+    auto labelArea = area.removeFromTop(labelHeight);
+    label.setBounds(labelArea.reduced(labelArea.getWidth() / 4, 0));
+
+    // Now the area variable has been reduced from the top by the height of the label
+    slider.setBounds(area); // The remaining area is for the slider
+}
+
 //==============================================================================
 MelloAudioProcessorEditor::MelloAudioProcessorEditor(MelloAudioProcessor &p, juce::AudioProcessorValueTreeState &vts)
-    : AudioProcessorEditor(&p), processorRef(p), processorState(vts)
+    : AudioProcessorEditor(&p),
+      processorRef(p),
+      processorState(vts),
+      sMix(ParameterID::kMix, "Mix", vts)
 {
     juce::ignoreUnused(processorRef);
 
-    for (auto &config : sliderConfigs)
-    {
-        createSlider(*this, config.slider, config.attachment, config.label, config.labelText, config.paramID);
-    }
+    addAndMakeVisible(sMix);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize(530, 300);
@@ -23,40 +61,6 @@ MelloAudioProcessorEditor::~MelloAudioProcessorEditor()
 
 bool MelloAudioProcessorEditor::isResizable() { return true; }
 
-void MelloAudioProcessorEditor::createSlider(juce::Component &target,
-                                             juce::Slider *slider,
-                                             std::unique_ptr<SliderAttachment> *attachment,
-                                             juce::Label *label,
-                                             const juce::String labelText,
-                                             const juce::String paramID)
-{
-    label->setText(labelText, juce::dontSendNotification);
-    label->attachToComponent(slider, false);
-    label->centreWithSize(5, 5);
-    slider->setSliderStyle(juce::Slider::SliderStyle::Rotary);
-    slider->setTextBoxStyle(juce::Slider::NoTextBox, false, 45, 0);
-    slider->setPopupDisplayEnabled(true, true, &target);
-    target.addAndMakeVisible(*label);
-    target.addAndMakeVisible(*slider);
-    attachment->reset(new SliderAttachment(processorState, paramID, *slider));
-}
-
-void MelloAudioProcessorEditor::createToggle(juce::Component &target,
-                                             juce::ToggleButton &button,
-                                             std::unique_ptr<ButtonAttachment> &attachment,
-                                             const juce::String &buttonText,
-                                             const juce::String &paramID)
-{
-
-    button.setButtonText(buttonText);
-    target.addAndMakeVisible(button);
-    attachment.reset(new ButtonAttachment(processorState, paramID, button));
-}
-
-void MelloAudioProcessorEditor::sliderValueChanged(juce::Slider *slider)
-{
-}
-
 //==============================================================================
 void MelloAudioProcessorEditor::paint(juce::Graphics &g)
 {
@@ -68,37 +72,6 @@ void MelloAudioProcessorEditor::paint(juce::Graphics &g)
 
 void MelloAudioProcessorEditor::resized()
 {
-    // Define the flexbox layout
-    juce::FlexBox fb;
-    fb.flexDirection = juce::FlexBox::Direction::column;       // Arrange components in rows
-    fb.flexWrap = juce::FlexBox::Wrap::wrap;                   // Allow wrapping onto new lines
-    fb.justifyContent = juce::FlexBox::JustifyContent::center; // Center items on the main-axis
-    fb.alignContent = juce::FlexBox::AlignContent::stretch;    // Stretch items to fill the cross-axis
 
-    // Define the dimensions of sliders based on the editor size
-    const float sliderWidth = getWidth() / 3.0f - 20.0f;   // Divide width into 3 columns
-    const float sliderHeight = getHeight() / 3.0f - 20.0f; // Divide height into 3 rows
-
-    // Add sliders to the flexbox
-    for (auto &config : sliderConfigs)
-    {
-        // Set up flex item properties
-        juce::FlexItem flexItem(sliderWidth, sliderHeight);
-        flexItem.margin = 5;                          // Set margin around items
-        flexItem.associatedComponent = config.slider; // Associate the slider with the flex item
-        fb.items.add(flexItem);
-
-        // Adjust label positions
-        config.label->setBounds(config.slider->getX(), config.slider->getY() - 10, sliderWidth, 20);
-    }
-
-    // Perform layout calculation
-    fb.performLayout(getLocalBounds().reduced(5)); // Apply the layout within the editor bounds
-
-    // Now manually set the bounds for each label since they are not part of the FlexBox items
-    for (auto &config : sliderConfigs)
-    {
-        auto sliderBounds = config.slider->getBounds();
-        config.label->setBounds(sliderBounds.getX(), sliderBounds.getY() - 10, sliderBounds.getWidth(), 20);
-    }
+    sMix.setBounds(100, 100, getWidth() * 0.3, getHeight() * 0.3);
 }
